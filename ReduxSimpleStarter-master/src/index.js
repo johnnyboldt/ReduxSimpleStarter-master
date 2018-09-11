aa409429a0405ec8ca1d6524554fcbce02654d44
johnnyboldt <johnny.boldt@gmail.com>
2018-09-11 16:07:21 -0600

    Hello world function

--- ReduxSimpleStarter-master/src/index.js
+++ ReduxSimpleStarter-master/src/index.js
@@ -1,3 +1,5 @@
 // Create a new component. This component should produce some HTML
-
+const App = function() {
+    return <div>Hello World!</div>;
+}
 // Take this component's generated HTML and put it on the page (in the DOM)
