4be8ba1f80b1317f96c7c756a37825cd005d2df2
johnnyboldt <johnny.boldt@gmail.com>
2018-09-17 11:18:15 -0600

    Hello world stuff and adding components

--- ReduxSimpleStarter-master/src/index.js
+++ ReduxSimpleStarter-master/src/index.js
@@ -1,5 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom';
+
 // Create a new component. This component should produce some HTML
-const App = function() {
+const App = () => {
     return <div>Hello World!</div>;
 }
+
+const App2 = () => {
+    return <div>Hello World2!</div>;
+}
+
+
 // Take this component's generated HTML and put it on the page (in the DOM)
+ReactDOM.render(<App />, document.querySelector('.container'));
+ReactDOM.render(<App2 />, document.querySelector('.container2'));
