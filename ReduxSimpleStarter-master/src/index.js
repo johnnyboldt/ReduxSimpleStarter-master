bb839072272429e7b793b266a4c7f3c9fddc3c56
johnnyboldt <johnny.boldt@gmail.com>
2018-09-18 14:00:04 -0600

    Tweaks

--- ReduxSimpleStarter-master/src/index.js
+++ ReduxSimpleStarter-master/src/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash'
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search'
@@ -14,6 +15,7 @@ class App extends Component{
             videos: [], 
             selectedVideo: null
         };
+        
         this.videoSearch('surfboards')
     }
 
@@ -27,9 +29,10 @@ class App extends Component{
     }
 
     render(){
+        const videoSearch = _.debounce((searchTerm) => {this.videoSearch(searchTerm)}, 300)
         return (
             <div>
-                <SearchBar onSearchTermChange={searchTerm => this.videoSearch(searchTerm)}/>
+                <SearchBar onSearchTermChange={videoSearch}/>
                 <VideoDetail video={this.state.selectedVideo} />
                 <VideoList 
                 onVideoSelect={selectedVideo => this.setState({selectedVideo})}
