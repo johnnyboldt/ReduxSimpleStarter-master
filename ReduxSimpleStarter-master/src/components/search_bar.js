44865b90268c6ce0d1f0711ec7a3d3a93464f50c
johnnyboldt <johnny.boldt@gmail.com>
2018-09-18 13:20:51 -0600

    Basic youtube player done

--- ReduxSimpleStarter-master/src/components/search_bar.js
+++ ReduxSimpleStarter-master/src/components/search_bar.js
@@ -4,17 +4,21 @@ class SearchBar extends Component {
     constructor(props){
         super(props);
 
-        this.state = { searchTerm: 'Initial Value' };
+        this.state = { searchTerm: '' };
     }
     render() {
         return (
-            <div>
+            <div className="search-bar">
                 <input 
                 value={this.state.searchTerm} 
-                onChange = {event => this.setState({ searchTerm: event.target.value })}/>
-                Value of the input: {this.state.searchTerm}
+                onChange = {event => this.onInputChange(event.target.value)}/>
             </div>
         );
+    }
+
+    onInputChange(searchTerm) {
+        this.setState({searchTerm});
+        this.props.onSearchTermChange(searchTerm);
     }
 }
 
